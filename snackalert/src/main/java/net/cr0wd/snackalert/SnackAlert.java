58327e65aab113d38f2b814b145ec3fbbefb37d8
Wolfhard Prell <wolfhard.prell@gmail.com>
2016-03-09 22:07:33 +0100

    added doc comments

--- snackalert/src/main/java/net/cr0wd/snackalert/SnackAlert.java
+++ snackalert/src/main/java/net/cr0wd/snackalert/SnackAlert.java
@@ -8,26 +8,62 @@ import android.widget.TextView;
 
 public class SnackAlert {
 
+    /**
+     * Shows success Snackbar for short period.
+     *
+     * @param view    The view to find a parent from.
+     * @param message The text to show. Can be formatted text.
+     */
     public static void success(View view, String message) {
         createSnackbar(view, message, R.color.success_background, R.color.success_text, Snackbar.LENGTH_SHORT).show();
     }
 
+    /**
+     * Shows error Snackbar for short period.
+     *
+     * @param view    The view to find a parent from.
+     * @param message The text to show. Can be formatted text.
+     */
     public static void error(View view, String message) {
         createSnackbar(view, message, R.color.error_background, R.color.error_text, Snackbar.LENGTH_SHORT).show();
     }
 
+    /**
+     * Shows info Snackbar for short period.
+     *
+     * @param view    The view to find a parent from.
+     * @param message The text to show. Can be formatted text.
+     */
     public static void info(View view, String message) {
         createSnackbar(view, message, R.color.info_background, R.color.info_text, Snackbar.LENGTH_SHORT).show();
     }
 
+    /**
+     * Shows success Snackbar for long period.
+     *
+     * @param view    The view to find a parent from.
+     * @param message The text to show. Can be formatted text.
+     */
     public static void longSuccess(View view, String message) {
         createSnackbar(view, message, R.color.success_background, R.color.success_text, Snackbar.LENGTH_LONG).show();
     }
 
+    /**
+     * Shows error Snackbar for long period.
+     *
+     * @param view    The view to find a parent from.
+     * @param message The text to show. Can be formatted text.
+     */
     public static void longError(View view, String message) {
         createSnackbar(view, message, R.color.error_background, R.color.error_text, Snackbar.LENGTH_LONG).show();
     }
 
+    /**
+     * Shows info Snackbar for long period.
+     *
+     * @param view    The view to find a parent from.
+     * @param message The text to show. Can be formatted text.
+     */
     public static void longInfo(View view, String message) {
         createSnackbar(view, message, R.color.info_background, R.color.info_text, Snackbar.LENGTH_LONG).show();
     }
